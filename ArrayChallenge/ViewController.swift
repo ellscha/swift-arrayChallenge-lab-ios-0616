0910e1981711f8bea2c297b67d6bc7bbf449d8d9
ellscha <escharlin@wesleyan.edu>
2016-07-07 10:34:36 -0400

    tests are passing but bundle is missing so it's not working.

--- ArrayChallenge/ViewController.swift
+++ ArrayChallenge/ViewController.swift
@@ -17,9 +17,23 @@ class ViewController: UIViewController {
         
     
         
-        
+        var quantities : [Int] = [6, 4, 12, 4]
+        var list : [String] = ["Bananas", "Apples", "Eggs", "Rolls"]
+             shoppingList = makeShoppingList(list, quantityOfItems: quantities)
+
     }
     
     
-    
+    func makeShoppingList(itemsNeeded: [String], quantityOfItems:[Int]) -> [String] {
+        var shopList : [String] = []
+        var items : [String] = []
+        for (index, item) in itemsNeeded.enumerate() {
+                
+                shopList.append("\(index + 1). \(quantityOfItems[index]) \(item)")
+            
+        }
+        print(shopList)
+        return shopList
+    }
+
 }
